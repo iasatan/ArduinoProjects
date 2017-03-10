5d4b58d44a337bad9711ba50ac453e39e4996168
Sátán Ádám <satanadam@outlook.com>
2017-03-10 18:19:43 +0100

    Update CatToy.ino

--- CatToy.ino
+++ CatToy.ino
@@ -13,29 +13,20 @@ short megarand;
 byte rand1;
 byte rand2;
 
-void setup() 
-{
-  vert.attach(vertpin);
-  hor.attach(horpin);  
-}
+void setup(){}
 
-void migrate(Servo &myServo, int newPos)
-{
+void migrate(Servo &myServo, int newPos){
   wait=random(0,20);
   pos = myServo.read();
-  if (pos < newPos)
-  { 
-    for (i=pos; i < newPos; i++)
-    {
+  if (pos < newPos)  { 
+    for (i=pos; i < newPos; i++)    {
       myServo.write(i);
       delay(wait); 
     }
     myServo.detach();
   } 
-  else 
-  { 
-    for (i=pos; i > newPos; i--) 
-    { 
+  else   { 
+    for (i=pos; i > newPos; i--)    { 
       myServo.write(i);
       delay(wait);  
     }
@@ -44,13 +35,12 @@ void migrate(Servo &myServo, int newPos)
 }
   
 void loop() { 
-  megarand=random(100,800);
+  randTime=random(100,800);
   vert.attach(vertpin);
   hor.attach(horpin); 
   rand1=random(150, 180);
   rand2=random(0,30);
   migrate(hor, rand1);
   migrate(vert, rand2);
-  delay(megarand);
+  delay(randTime);
 }
-
