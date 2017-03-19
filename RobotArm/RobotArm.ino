8e161aff4c77b5017fe77a61c1edf124a4184c14
Sátán Ádám <satanadam@outlook.com>
2017-03-19 14:30:01 +0100

    Update RobotArm.ino

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -1,6 +1,5 @@
 #include <Servo.h>
-
-#define Wait 15
+#include <robotarm.h>
 
 Servo clawServo;  
 Servo handServo;      
@@ -21,96 +20,6 @@ void setup()
   initServos();
 }
 
-void initServos(){
-  clawServo.attach(9);
-  handServo.attach(8);
-  elbowServo.attach(10);
-  tiltServo.attach(11);
-  panServo.attach(12);
-  clawServo.write(140);
-  handServo.write(180);
-  elbowServo.write(90);
-  panServo.write(90);
-  tiltServo.write(90);
-}
-
-void shake(byte times){
-  for(int i=0;i<times;i++){
-      migrate(panServo, 130, 10);
-      migrate(panServo, 70, 10);
-      delay(100);
-  }
-  migrate(panServo, 100, 10);
-}
-
-void armSwing(){
-  migrate(tiltServo, 180, 10);
-  migrate(handServo, 90, 10);
-  migrate(elbowServo, 0, 10);
-  migrate(elbowServo, 10, 10);
-  migrate(tiltServo, 50, 10);
-}
-
-void highFive(byte serialPos){
-  shake(serialPos);
-  migrate(tiltServo, 180, 10);
-  migrate(handServo, 90, 10);
-  migrate(elbowServo, 0, 10);
-  migrate(elbowServo, 10, 10);
-  migrate(tiltServo, 50, 10);
-}
-
-void wave(int times){
-  elbowServo.write(20);
-  for(int k=0;k<times;k++){
-    migrate(handServo,70, Wait);
-    Serial.println(k);
-    migrate(handServo,130, Wait);
-  }
-}
-
-void masturbate(byte  times){
-  tiltServo.write(90);
-  int z, u;
-  for(int l=0; l<times;l++){
-    for(z=70, u=30; z<130;z++, u++){
-      handServo.write(u);
-      elbowServo.write(z);
-      delay(14);
-    }
-  }
-}
-void rest(){
-  migrate(panServo,90,Wait);
-  migrate(elbowServo,180, Wait);
-  migrate(tiltServo,180, Wait);
-  migrate(handServo,180, Wait);
-  tiltServo.detach();
-  panServo.detach();
-  elbowServo.detach();
-  handServo.detach();
-}
-  
-void migrate(Servo myServo, byte newPos, byte wait)
-{
-  byte pos=myServo.read(); //reads the current position of the servo
-  if (pos < newPos)
-  {
-    for (int i = pos; i < newPos; i++)
-    {
-      myServo.write(i);
-      delay(wait);
-    }
-  }
-  else
-  {
-    for (int i = pos; i > newPos; i--)
-    {
-      myServo.write(i);
-      delay(wait);
-    }
-  }
-}
 void loop()
 {
   byte hundredByte;
@@ -142,16 +51,16 @@ void loop()
       case 'c': //claw
         Serial.println("Claw");
         serialPos = constrain(serialPos, 66, 140);
-        migrate(clawServo,serialPos, Wait);
+        migrate(clawServo,serialPos, 15);
         break;
       case 'h': //hand
         Serial.println("hand");
-        migrate(handServo,serialPos, Wait);
+        migrate(handServo,serialPos, 15);
         break;
       case 'e': //elbow
         Serial.println("elbow");
         serialPos = constrain(serialPos, 0, 155);
-        migrate(elbowServo,serialPos, Wait);
+        migrate(elbowServo,serialPos, 15);
         break;
       case 'p': //shoulderPan
         Serial.println("shoulderPan");
@@ -159,24 +68,109 @@ void loop()
         break;
       case 't': //shoulderTilt
         Serial.println("shoulderTilt");
-        migrate(tiltServo,serialPos,Wait);
+        migrate(tiltServo,serialPos,15);
         break;
       case 'r': //resting
+        Serial.println("resting");
         rest();
         break;
       case 's': //start
+        Serial.println("start");
         initServos();
         break;
       case 'w': //wave
+        Serial.println("wave");
         wave(serialPos);
         break;
       case 'm': //masturbate
+        Serial.println("masturbate");
         masturbate(serialPos);
         break;
       case 'f': //highFive
+        Serial.println("highFive");
         highFive(serialPos);
         break;
+      case 'n': //Nazi ArmSwing
+        Serial.println("armSwing");
+        armSwing();
+        break;
+      case 'q':
+        Serial.println("test");
+        migrate(panServo, 0, handServo, 90, 10);
   }
   serialServo=' ';
   delay(100);
 }
+
+void rest(){
+  migrate(handServo, 180, elbowServo, 180, tiltServo, 180, panServo, 90, 15);
+  delay(100);
+  tiltServo.detach();
+  panServo.detach();
+  elbowServo.detach();
+  handServo.detach();
+}
+
+void masturbate(byte times){
+  tiltServo.write(90);
+  for (int l = 0; l<times; l++) {
+    migrate(handServo, 80, elbowServo, 80, 18);
+    migrate(handServo, 40, elbowServo, 130, 18);
+  }
+}
+
+void wave(byte times){
+  migrate(panServo, 0, elbowServo, 20, 10);
+  //migrate(panServo, 0, 10);
+  //elbowServo.write(20);
+  for (int k = 0; k<times; k++) {
+    migrate(handServo, 70, 15);
+    migrate(handServo, 130, 15);
+  }
+}
+
+void shake(byte times){
+  migrate(handServo, 180, 10);
+  for (int i = 0; i<times; i++) {
+    migrate(panServo, 130, 10);
+    migrate(panServo, 70, 10);
+    delay(100);
+  }
+  migrate(panServo, 100, 10);
+}
+
+void highFive(byte times){
+  shake(times);
+  migrate(tiltServo, 180, 10);
+  migrate(handServo, 90, 10);
+  migrate(elbowServo, 0, 10);
+  migrate(elbowServo, 10, 10);
+  migrate(tiltServo, 50, 10);
+  migrate(tiltServo, 180, 10);
+  migrate(tiltServo, 50, 25);
+  delay(1000);
+  initServos();
+}
+
+void armSwing(){
+  migrate(panServo, 0, 10);
+  migrate(tiltServo, 0, 10);
+  migrate(elbowServo, 0, 10);
+  migrate(handServo, 90, 10);
+  migrate(elbowServo, 10, 10);
+  migrate(tiltServo, 60, 10);
+}
+
+void initServos(){
+  clawServo.attach(9);
+  handServo.attach(8);
+  elbowServo.attach(10);
+  tiltServo.attach(11);
+  panServo.attach(12);
+  clawServo.write(140);
+  handServo.write(180);
+  elbowServo.write(90);
+  panServo.write(90);
+  tiltServo.write(100);
+}
+
